0d02190cd722dd47f7b9ca2b1bbd054f6ead4806
Karncita <karensad66@gmail.com>
2025-02-24 03:47:53 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const amigos = [];
+ function agregarAmigo() {
+    const nombreEntrada = document.getElementById("amigo");
+    const nombre = nombreEntrada.value.trim();
+
+    if (nombre === ""){
+        alert("Por favor,ingrese un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    nombreEntrada.value = "";
+
+ }
+
+ function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach((amigo,index) => {
+        const li = document.createElement("li");
+        li.textContent= amigo;
+        lista.appendChild(li);
+    });
+
+ }
+
+ function sortearAmigo(){
+    if (amigos.length === 0){
+        alert("No hay nombres en la lista para sortear.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random()* amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+
+    const resultado =document.getElementById("resultado");
+    resultado.innerHTML = `<li> El amigo secreto es:<strong>${amigoSorteado}</strong> </li>`;
+ }    
